4b6c97e54bee7ec10ef10543bbb46090091ead86
Aditya Kulshrestha <adi.kul358@gmail.com>
2023-05-30 00:33:03 +0530

    Meta Tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,6 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'NexusCalc',
-}
-
 export default function RootLayout({
   children,
 }: {
@@ -15,6 +11,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <title>NexusCalc — Project by Aditya Kulshrestha</title>
+      <meta name="title" content="NexusCalc — Project by Aditya Kulshrestha" />
+      <meta name="description" content="NexusCalc — Project by Aditya Kulshrestha" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://nexuscalc.adikul.dev/" />
+      <meta property="og:title" content="NexusCalc — Project by Aditya Kulshrestha" />
+      <meta property="og:description" content="NexusCalc — Project by Aditya Kulshrestha" />
+      <meta property="og:image" content="https://nexuscalc.adikul.dev/banner.png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://nexuscalc.adikul.dev/" />
+      <meta property="twitter:title" content="NexusCalc — Project by Aditya Kulshrestha" />
+      <meta property="twitter:description" content="NexusCalc — Project by Aditya Kulshrestha" />
+      <meta property="twitter:image" content="https://nexuscalc.adikul.dev/banner.png" />
+
         <link rel="icon" type="image/x-icon" href="/favicon.png"/>
       </head>
       <body className={inter.className}>
